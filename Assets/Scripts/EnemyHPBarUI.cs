bd7a23cc602696f2bb1993b8e7511fe45842efee
Daisuke <sdt2495gp0006@edu.sca.ac.jp>
2026-05-25 19:10:11 +0900

    EnemyHPバーUI追加

---
+++ Assets/Scripts/EnemyHPBarUI.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EnemyHPBarUI : MonoBehaviour
+{
+    [Header("EnemyHP")]
+    [SerializeField] private EnemyHealth enemyHealth;
+
+    [Header("前面HPバー")]
+    [SerializeField] private Image frontUI;
+
+    [Header("メインカメラ")]
+    [SerializeField] private Camera mainCamera;
+
+    private void Update()
+    {
+        // HP割合更新
+        frontUI.fillAmount = (float)enemyHealth.CurrentHP / enemyHealth.MaxHP;
+
+        // カメラ方向を向く
+        transform.forward = mainCamera.transform.forward;
+    }
+}
